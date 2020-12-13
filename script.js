2d43c0058a2eb5b770fe14d7a2b840d02501c82d
amy-designs <71774293+amy-designs@users.noreply.github.com>
2020-12-13 17:59:30 -0500

    mobile menu

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+var width = window.innerWidth;
+var height = window.innerHeight;
+
+
 //Greensock Animations//
 jQuery(function() {
     gsap.set($(".GSAPBody").add(".GSAPLowerBody"), {css: {opacity: 0}});
@@ -5,74 +9,122 @@ jQuery(function() {
                                                    duration: 1,
                                                    delay: 1});
 
-    var prog = gsap.to($("#progPort"), {css: {color: "red"},
+    gsap.to($("#PortNav"), {css: {opacity: 0},
                                 scrollTrigger: {
                                     trigger: "#scrollHeader",
                                     start: "top center",
-                                    paused: true
+                                    toggleActions: "restart none none reverse"
                                 },
-                                duration: 1});
+                                duration: 0.3});
 
-    const playST = ScrollTrigger.create({
-                                trigger:"#scrollHeader", 
-                                start:'top center', 
-                                onEnter: () => prog.play()
-    });
+    gsap.to($("#progPort"), {css: {color: "white", opacity: 1},
+                                scrollTrigger: {
+                                    trigger: "#scrollHeader",
+                                    start: "top center",
+                                    toggleActions: "restart none none reverse"
+                                },
+                                duration: 0.3});
 
-    const resetST = ScrollTrigger.create({
-                                trigger:"#scrollHeader",
-                                start:'top center',
-                                onLeaveBack: () => prog.pause(0)
-    });
-    $(".NavText").on("mouseenter", function() {
-        gsap.to($(this), {css: {fontSize: "1em"},
-                            duration: 0.25});
-    });
+    gsap.to($("#progAbout"), {css: {color: "white", opacity: 0.7},
+                                scrollTrigger: {
+                                    trigger: "#scrollHeader",
+                                    start: "top center",
+                                    toggleActions: "restart none none reverse"
+                                },
+                                duration: 0.3});
 
-    $(".NavText").on("mouseleave", function() {
-        gsap.to($(this), {css: {fontSize: "0.8em"},
-                            duration: 0.25});
+    gsap.to($("#progSide"), {css: {color: "white", opacity: 0.7},
+                                scrollTrigger: {
+                                    trigger: "#scrollHeader",
+                                    start: "top center",
+                                    toggleActions: "restart none none reverse"
+                                },
+                                duration: 0.3});
 
-    });
+    if (width > 1024) {
 
-    $(".progText").on("mouseenter", function() {
-        gsap.to($(this), {css: {fontSize: "1rem"},
-                            duration: 0.25});
-    });
+        $(".progText").on("mouseenter", function() {
+            gsap.to($(this), {css: {fontSize: "1.15vw"},
+                                duration: 0.25});
+        });
 
-    $(".progText").on("mouseleave", function() {
-        gsap.to($(this), {css: {fontSize: "0.65rem"},
-                            duration: 0.25});
+        $(".progText").on("mouseleave", function() {
+            gsap.to($(this), {css: {fontSize: "1vw"},
+                                duration: 0.25});
 
-    });
+        });
+        $(".NavText").on("mouseenter", function() {
+            gsap.to($(this), {css: {fontSize: "1.1vw"},
+                                duration: 0.25});
+        });
 
-})
+        $(".NavText").on("mouseleave", function() {
+            gsap.to($(this), {css: {fontSize: "1vw"},
+                                duration: 0.25});
+
+        });
+
+        $(".proj").on("mouseenter", function() {
+            gsap.to($(this), {css: {width: "61vw"},
+                                duration: 0.25});
+        });
+
+        $(".proj").on("mouseleave", function() {
+            gsap.to($(this), {css: {width: "60vw"},
+                                duration: 0.25});
+
+        });
+    }
+
+    $(document).ready(function() {
+  
+        $(".menu-link").click(function(event) {
+          event.preventDefault();
+          $(".menu-overlay").toggleClass("open");
+          $(".menu").toggleClass("open");
+      
+        });
+          
+      });
+    
+});
 
 
 //Cursor Animation//
-document.onmousemove = animateCircles;
-
-function animateCircles(event) {
-    var circle = document.createElement('div');
-    circle.setAttribute('class', 'circle');
+if (width > 1024) {
+    var circle = document.createElement('span');
+    circle.id="mouse-circle";
+    circle.className = "mouse-circle";
     document.body.appendChild(circle);
 
-    circle.style.left = event.clientX + 'px';
-    circle.style.top = event.clientY + 'px';
+    document.addEventListener('DOMContentLoaded', () => {
+        let mousePosX = 0,
+            mousePosY = 0,
+            mouseCircle = document.getElementById('mouse-circle');
 
-    circle.style.transition = "all 0.5s linear 0s";
+        document.onmousemove = (e) => {
+            mousePosX = e.pageX;
+            mousePosY = e.pageY;
+        }
 
-    circle.style.left = circle.offsetLeft-35+'px';
-    circle.style.top = circle.offsetTop-35+'px';
-    circle.style.width = '35px';
-    circle.style.height= '35px';
-    circle.style.borderWidth = '4px';
-    circle.style.opacity = 0;
+        let delay = 6,
+            revisedMousePosX = 0,
+            revisedMousePosY = 0;
 
-    setTimeout(() => {
-        circle.remove();
-        }, 300);
-};
+        function delayMouseFollow() {
+            requestAnimationFrame(delayMouseFollow);
+
+            revisedMousePosX += (mousePosX - revisedMousePosX) / delay;
+            revisedMousePosY += (mousePosY - revisedMousePosY) / delay; 
+
+            mouseCircle.style.top = revisedMousePosY + 'px';
+            mouseCircle.style.left = revisedMousePosX + 'px';
+        }
+        delayMouseFollow();
+    });
+}
+
+
 
 
 
